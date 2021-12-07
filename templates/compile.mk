6d14083a5e963a870968c160edac921fecd4ac2f
Supdrewin <supdrewin@outlook.com>
2021-12-07 14:18:16 +0800

    Update modules

---
+++ templates/compile.mk
@@ -0,0 +1,12 @@
+CXX      := clang++
+CXXFLAGS += -std=c++2a -stdlib=libc++
+CXXFLAGS += -I$(PREFIX)/include
+CXXFLAGS += -finline-functions
+CXXFLAGS += -Wall -Weffc++ -Wextra -Wsign-conversion -pedantic-errors
+CXXFLAGS +=	-pipe -glldb -O2 -o $(PREFIX)/$@
+LDFLAGS  += -fuse-ld=lld -flto=thin -L$(PREFIX)/lib
+LDFLAGS  += -lassimp -ldl -lfreetype -lglad -lglfw3 -lpthread -lzlibstatic
+
+bin/$(PROJECT): *.cpp
+	$(CXX) $(CXXFLAGS) $^ $(LDFLAGS)
+.PHONY: bin/$(PROJECT)
