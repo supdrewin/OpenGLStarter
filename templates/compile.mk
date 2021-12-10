7927355402c4ed13aac02bfe208777e312375926
Supdrewin <supdrewin@outlook.com>
2021-12-10 12:19:34 +0800

    More compiler/linker's flags

--- templates/compile.mk
+++ templates/compile.mk
@@ -1,11 +1,12 @@
 CXX      := clang++
-CXXFLAGS += -std=c++2a -stdlib=libc++
-CXXFLAGS += -I$(PREFIX)/include
-CXXFLAGS += -finline-functions
+
+CXXFLAGS += -std=c++2a -stdlib=libc++ -I$(PREFIX)/include
+CXXFLAGS += -flto=thin -finline-functions -fstack-protector-strong
 CXXFLAGS += -Wall -Weffc++ -Wextra -Wsign-conversion -pedantic-errors
 CXXFLAGS +=	-pipe -glldb -O2 -o $(PREFIX)/$@
-LDFLAGS  += -fuse-ld=lld -flto=thin -L$(PREFIX)/lib
-LDFLAGS  += -lassimp -ldl -lfreetype -lglad -lglfw3 -lpthread -lstb -lzlibstatic
+
+LDFLAGS  += -fpie -fuse-ld=lld -rtlib=compiler-rt -unwindlib=libunwind -L$(PREFIX)/lib
+LDFLAGS  += -lassimp -ldl -lfreetype -lgcc -lglad -lglfw3 -lpthread -lstb -lzlibstatic
 
 bin/$(PROJECT): *.cpp
 	$(CXX) $(CXXFLAGS) $^ $(LDFLAGS)
